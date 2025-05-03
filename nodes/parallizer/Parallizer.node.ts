9704363fe82ae75dcd9b350c9e1c162ed61b37cd
d1ndonlymdhe <ayushshrestha8920@protonmail.com>
2025-05-03 19:48:56 +0545

    add credentials

--- nodes/parallizer/Parallizer.node.ts
+++ nodes/parallizer/Parallizer.node.ts
@@ -1,43 +1,53 @@
-import type { IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription } from "n8n-workflow";
+import type {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
 
 export class Parallizer implements INodeType {
-	description:INodeTypeDescription =  {
-		displayName: "Parallizer",
-		name: "parallizer",
-		icon: "file:parallizer.svg",
-		group: ["transform"],
+	description: INodeTypeDescription = {
+		displayName: 'Parallizer',
+		name: 'parallizer',
+		icon: 'file:parallizer.svg',
+		group: ['transform'],
 		version: 1,
-		description: "Parallize the execution of the workflow",
+		description: 'Parallize the execution of the workflow',
 		defaults: {
-			name: "Parallizer",
+			name: 'Parallizer',
 		},
-		inputs: ["main"],
-		outputs: ["main"],
-		properties: [{
-			displayName:"Requests",
-			name:"resource",
-			type:"json",
-			default: "",
-			required: true,
-
-		}],
+		inputs: ['main'],
+		outputs: ['main'],
+		properties: [
+			{
+				displayName: 'Requests',
+				name: 'resource',
+				type: 'json',
+				default: '',
+				required: true,
+			},
+		],
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
-		const requests = this.getNodeParameter("resource", 0) as string;
-		const parsedRequests = JSON.parse(requests) as { url: string, body: string;method:"GET"|"POST"|"PUT"|"PATCH"|"DELETE" }[];
+		const requests = this.getNodeParameter('resource', 0) as string;
+		const parsedRequests = JSON.parse(requests) as {
+			url: string;
+			body: string;
+			method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+		}[];
 		const returnItems = [];
 		for (const _ of items) {
-			const results: INodeExecutionData[] = await Promise.all(parsedRequests.map(async (req) => {
-				return this.helpers.httpRequest(
-					{
+			const results: INodeExecutionData[] = await Promise.all(
+				parsedRequests.map(async (req) => {
+					return this.helpers.httpRequest({
 						url: req.url,
 						method: req.method,
 						body: req.body,
-					}
-				)
-			}))
+					});
+				}),
+			);
 			returnItems.push({
 				results: results,
 			});
